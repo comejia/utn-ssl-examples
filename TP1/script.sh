f21f1951df338f6780a0ca81471c12d251635803
cmejia <comejia@teco.com.ar>
2021-05-27 21:11:31 -0300

    Actualizacion de comandos en distintos puntos

--- TP1/script.sh
+++ TP1/script.sh
@@ -3,19 +3,19 @@
 # Comandos del punto 2
 
 # a
-sed 's/\./.\n/g' breve_historia.txt
+sed 's/\./\.\n/g' breve_historia.txt
 
 # b
-sed 's/\./.\n/g' breve_historia.txt | sed '/^\s*$/d'
+sed 's/\./\.\n/g' breve_historia.txt | sed '/^\s*$/d'
 
 # c
-sed 's/\./.\n/g' breve_historia.txt | sed '/^\s*$/d' > breve_historia_2.txt
+sed 's/\./\.\n/g' breve_historia.txt | sed '/^\s*$/d' > breve_historia_2.txt
 
 # d
-grep -i --color 'guerra' breve_historia.txt
+grep -iF --color 'guerra' breve_historia.txt
 
 # e
-grep --color -E '^A.*(s|s\.)$' breve_historia.txt
+sed 's/\./\.\n/g' breve_historia.txt | grep --color -E '^A.*(s|s\.)$'
 
 # f
 grep --color -c 'peronismo' breve_historia.txt
@@ -28,4 +28,7 @@ grep --color -E '18[0-9]{2}' breve_historia.txt
 
 # i
 sed 's/^[a-zA-Z]*\b//g' breve_historia.txt
+
+# j
+ls . | grep --color -nE '.*\.txt'
 
