5aab07ee1580800992ff673a78711b256bcd3590
cmejia <comejia@teco.com.ar>
2021-09-05 21:20:53 -0300

    Parsea de datos

--- TP2/procesador.c
+++ TP2/procesador.c
@@ -10,6 +10,7 @@ static char pagina[1048576];
 static void obtenerTabla(FILE *fd);
 static void generarReporte(t_tabla *tabla, REPORTE reporte, FORMATO tipo);
 static t_tabla *parsearTabla(char *pagina);
+static void guardarDato(t_registro *regs, int indice, char *dato);
 
 
 void listarEspeciesEnNegativoPor(FILE *fd, FORMATO formato) {
@@ -27,7 +28,6 @@ static void generarReporte(t_tabla *tabla, REPORTE reporte, FORMATO tipo) {
 
 }
 
-
 static void obtenerTabla(FILE *fd) {
     char line[4096];
     while(fgets(line, sizeof(line), fd) != NULL) {
@@ -41,7 +41,7 @@ static void obtenerTabla(FILE *fd) {
         if(strstr(line, "</tbody>") != NULL) {
             break;
         }
-        if (strstr(line, ">48hs<")) {
+        if(strstr(line, ">48hs<")) {
             strcat(pagina, line);
         }
         
@@ -51,7 +51,88 @@ static void obtenerTabla(FILE *fd) {
 }
 
 static t_tabla *parsearTabla(char *pagina) {
-    t_tabla *tabla = malloc(sizeof(t_tabla));
+    t_tabla *tabla = (t_tabla *)malloc(sizeof(t_tabla));
+    tabla->regs = NULL;
+    tabla->filas = 0;
+    char *aux = pagina;
+    char *td = "<td";
+    char *registro;
+    
+    char dato[16];
+    int i;
+    int iDato = 0;
+
+    while ((registro = strstr(aux, td)) != NULL) {
+        (tabla->filas)++;
+        tabla->regs = realloc(tabla->regs, sizeof(t_registro) * tabla->filas);
+        iDato = 0;
+        while ((registro = strstr(aux, td)) != NULL && iDato < 16) {
+            while (*registro++ != '>');
+            // PROCESAR DATO CON SPAN (PORCENTAJE DE VARIACION)
+            for(i = 0; registro[i] != '<'; i++) {
+                dato[i] = registro[i];
+            }
+            dato[i] = '\0';
+            guardarDato(&(tabla->regs[(tabla->filas)-1]), iDato, dato);
+            aux = registro;
+            iDato++;
+        }
+    }
+    //printf("FILAS: %d\n", tabla->filas);
+    //printf("DATO: %s\n", tabla->regs[1].min);
+
     return tabla;
 }
 
+static void guardarDato(t_registro *regs, int indice, char *dato) {
+    switch(indice) {
+        case 0:
+            strcpy(regs->especie, dato);
+            break;
+        case 1:
+            strcpy(regs->vencimiento, dato);
+            break;
+        case 2:
+            strcpy(regs->cantNominalCompra, dato);
+            break;
+        case 3:
+            strcpy(regs->precioCompra, dato);
+            break;
+        case 4:
+            strcpy(regs->precioVenta, dato);
+            break;
+        case 5:
+            strcpy(regs->cantNominalVenta, dato);
+            break;
+        case 6:
+            strcpy(regs->ultimo, dato);
+            break;
+        case 7:
+            strcpy(regs->variacion, dato);
+            break;
+        case 8:
+            strcpy(regs->apertura, dato);
+            break;
+        case 9:
+            strcpy(regs->min, dato);
+            break;
+        case 10:
+            strcpy(regs->max, dato);
+            break;
+        case 11:
+            strcpy(regs->cierreAnterior, dato);
+            break;
+        case 12:
+            strcpy(regs->volumen, dato);
+            break;
+        case 13:
+            strcpy(regs->monto, dato);
+            break;
+        case 14:
+            strcpy(regs->operacion, dato);
+            break;
+        case 15:
+            strcpy(regs->hora, dato);
+            break;
+    }
+}
--- TP2/procesador.h
+++ TP2/procesador.h
@@ -5,28 +5,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+// typedef struct {
+//     char especie[8];
+//     char vencimiento[8];
+//     int cantNominalCompra;
+//     double precioCompra;
+//     double precioVenta;
+//     int cantNominalVenta;
+//     double ultimo;
+//     double variacion;
+//     double apertura;
+//     double min;
+//     double max;
+//     double cierreAnterior;
+//     long volumen;
+//     long monto;
+//     int operacion;
+//     char hora[12];
+// } t_registro;
 typedef struct {
     char especie[8];
     char vencimiento[8];
-    int cantNominalCompra;
-    double precioCompra;
-    double precioVenta;
-    int cantNominalVenta;
-    double ultimo;
-    double variacion;
-    double apertura;
-    double min;
-    double max;
-    double cierreAnterior;
-    long volumen;
-    long monto;
-    int operacion;
+    char cantNominalCompra[8];
+    char precioCompra[8];
+    char precioVenta[8];
+    char cantNominalVenta[8];
+    char ultimo[8];
+    char variacion[8];
+    char apertura[8];
+    char min[8];
+    char max[8];
+    char cierreAnterior[8];
+    char volumen[16];
+    char monto[16];
+    char operacion[8];
     char hora[12];
 } t_registro;
 
 typedef struct {
     int filas;
-    int columnas;
+    //int columnas;
     t_registro *regs;
 } t_tabla;
 
